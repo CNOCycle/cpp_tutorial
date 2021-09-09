2f650a42d4a896c09dfddeebcdb26a97a6d5c40d
CNOCycle <github@marginalman.org>
2021-09-09 20:18:13 +0800

    Add files via upload

---
+++ 01-intro/01-hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    // declaration statement
+    int a = 1;
+    int b = 5;
+    int ans, n;
+    // expression statement
+    n = b - a + 1;
+    ans = (a + b) * n / 2;
+    std::cout << ans << std::endl;
+    // return statement
+    return 0;
+}
---
+++ 01-intro/02-bad_style.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){int a = 1;
+    int b = 5;
+int ans, n; n = b - a + 1;
+    ans = (a + b) * n / 2;
+    std::cout << ans << std::endl; return 0;
+}

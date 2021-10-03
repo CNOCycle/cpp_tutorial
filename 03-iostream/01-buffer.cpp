ed27345bb0a49521d4d60dd1c0695607e0236be0
CNOCycle <github@marginalman.org>
2021-10-03 13:36:17 +0800

    Add files via upload

--- 03-iostream/01-buffer.cpp
+++ 03-iostream/01-buffer.cpp
@@ -8,78 +8,38 @@ using namespace std;
 int main()
 {
     // output to the screen immediately
-    //for (int i = 1; i <= 5; ++i)
-    //{
-    //    std::std::cout << i << " " << std::flush;
-    //    this_thread::sleep_for(chrono::seconds(1));
-    //}
-    std::cout << 1 << " " << std::flush;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 2 << " " << std::flush;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 3 << " " << std::flush;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 4 << " " << std::flush;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 5 << " " << std::flush;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << endl;
+    for (int i = 1; i <= 5; ++i)
+    {
+        cout << i << " " << std::flush;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    cout << endl;
 
     // output at once
-    //for (int i = 1; i <= 5; ++i)
-    //{
-    //    cout << i << " ";
-    //    this_thread::sleep_for(chrono::seconds(1));
-    //}
-    std::cout << 1 << " " ;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 2 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 3 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 4 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 5 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << endl;
+    for (int i = 1; i <= 5; ++i)
+    {
+        cout << i << " ";
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    cout << endl;
 
     // disable buffer
     std::cout.setf(std::ios::unitbuf);
-    //for (int i = 1; i <= 5; ++i)
-    //{
-    //    cout << i << " ";
-    //    this_thread::sleep_for(chrono::seconds(1));
-    //}
-    std::cout << 1 << " " ;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 2 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 3 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 4 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 5 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << endl;
+    for (int i = 1; i <= 5; ++i)
+    {
+        cout << i << " ";
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    cout << endl;
 
     // unset buffer
     std::cout.unsetf(std::ios::unitbuf);
-    //for (int i = 1; i <= 5; ++i)
-    //{
-    //    cout << i << " ";
-    //    this_thread::sleep_for(chrono::seconds(1));
-    //}
-    std::cout << 1 << " " ;
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 2 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 3 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 4 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << 5 << " ";
-    this_thread::sleep_for(chrono::seconds(1));
-    std::cout << endl;
+    for (int i = 1; i <= 5; ++i)
+    {
+        cout << i << " ";
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    cout << endl;
 
   return 0;
 }
